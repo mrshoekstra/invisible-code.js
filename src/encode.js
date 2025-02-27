c5dfc4980d1f1a817d31038efca5c5409aecaed5
Marc Hoekstra <github@marchoekstra.nl>
2025-02-27 20:51:44 +0100

    Optimized encode

--- src/encode.js
+++ src/encode.js
@@ -1,20 +1,25 @@
-// Define JavaScript code to encode
+// Define JavaScript code string to encode
 const decodedCode = '';
 
 // Convert the characters into a Uint8Array (UTF-8 encoded bytes)
 const encodedBytes = new TextEncoder().encode(decodedCode);
 
-// Convert each byte into an 8-bit binary string using reduce
-const binaryString = encodedBytes.reduce((string, byte) => {
-	const binaryByte = byte
-		// Convert byte to binary
-		.toString(2)
-		// Pad with leading zeros to ensure 8-bit representation
-		.padStart(8, 0);
-
-	// Append the binary representation to the accumulated string
-	return string + binaryByte;
-}, '');
+// Convert each byte into an 8-bit binary string
+const binaryString = encodedBytes
+	// For each byte
+	.reduce((bytes, byte) => {
+		// Add every converted byte into an array
+		bytes.push(
+			byte
+				// Convert byte to binary
+				.toString(2)
+				// Pad with leading zeros to ensure 8-bit representation
+				.padStart(8, 0)
+		);
+		return bytes;
+	}, [])
+	// Merge array into a string
+	.join('');
 
 // Create a hidden string by replacing ones and zeros
 const encodedString = binaryString
@@ -24,4 +29,4 @@ const encodedString = binaryString
 	.replace(/1/g, '‍');
 
 // Log the hidden string to the console
-console.log("'" + encodedString + "'");
+console.log(`'${encodedString}'`);
